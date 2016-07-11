c98084b9c7bb10761b2790395fe2c89e6a4a965e
Isaac Yoon <realisy@gmail.com>
2016-07-11 14:41:04 -0400

    fixed url validation

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,6 +2,7 @@ class Track < ActiveRecord::Base
 
   validates :title, presence: true, length: { maximum: 40 }
   # validates :url, presence: true, length: { maximum: 140 }
+  validates_format_of :url, with: /\A(http(?:s)?).+/ , allow_blank: true
   validates :author, presence: true, length: { maximum: 25 }
 
 end
